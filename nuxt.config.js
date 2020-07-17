7d1a97c575f4e28492ed3975ea323fdea92eb5ef
marfru <hola@marfru.com>
2020-07-17 12:52:27 +0200

    adding html lang localization

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,9 @@ export default {
    */
   head: {
     title: process.env.TITLE_PORTFOLIO || '',
+    htmlAttrs: {
+      lang: 'en',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
